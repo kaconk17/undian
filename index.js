e2e8e4fa9cacae0b47561d0117d36557559d946d
kaconk17 <harutonouciha@gmail.com>
2024-07-02 15:50:37 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+app.set('view engine', 'ejs')
+//const bootstrap = require('bootstrap')
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+//app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/')));
+
+app.get('/', (req, res) => {
+  res.render('login');
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
